7d948b08a585182b97ea53b3f8e1b3541698b0d4
xeekworx <xeek@xeekworx.com>
2016-05-02 10:08:50 -0500

    Initial commit, but the README is not yet complete.

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# autoversion
+**Xeekworx Autoversion**
+------------------------
+
+**Autoversion is a command-line tool written in Python that looks for preprocessor definitions in a C/C++ header file and modifies the value to increment version values.**
+
+HOW IT WORKS
+------------
+
+As an example, if you give it the definition name *MYAPP_VERSION*, it will search (line by line) the header file for this *#define* and increment any version values it detects. Multiple definition names can be given.
+
+The format of the version should be, X.X.X.X, but each part is optional (so X.X is valid or even just X). Also the value separator can be a period or a comma and quotes (as for strings) is also fine. Below is an example of how autoversion interprets your version values:
+
+    MAJOR.MINOR.REVISION.BUILD or 1.16.52.1
+
+The version is incremented in this way ...
+
+ 1. **MAJOR**: This is not incremented unless it is the only value that exists in the version. If this is the only value in the version, it is incremented by + 1.
+ 2. **MINOR**: This is set to the two digit representation of the current year.
+ 3. **REVISION**: This is the month and day combined. If the current local date is May 2, the revision will be set to 52.
+ 4. **BUILD**: This is incremented by + 1.
+
+The version value separator, leading and trailing quotations, and trailing "\0" will be maintained when the version is modified.
+
+COMMAND LINE
+------------
+
+CONFIGURING VISUAL STUDIO
+-----------------------------------
+
+
+> Written with [StackEdit](https://stackedit.io/).
---
+++ autoversion.py
@@ -0,0 +1,124 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+__author__ = 'jtullos'
+__version__ = '1.16.52'
+
+# The MIT License (MIT)
+# Copyright (c) 2016 John Andrew Tullos (xeek@xeekworx.com)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+# software and associated documentation files (the "Software"), to deal in the Software 
+# without restriction, including without limitation the rights to use, copy, modify, 
+# merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+# permit persons to whom the Software is furnished to do so, subject to the following 
+# conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies 
+# or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+# CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+# OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import sys
+import argparse
+from datetime import *
+
+VERSION = __version__
+DEFAULT_NAME = 'autoversion'
+
+
+def main(argv = None):
+
+    # HANDLE COMMAND-LINE ARGUMENTS:
+    if not argv:
+        argv = sys.argv[1:]
+
+    parser = argparse.ArgumentParser(description = DEFAULT_NAME, 
+                                     add_help = False)
+    parser.add_argument('source',
+                        metavar='FILE', 
+                        type=str, 
+                        nargs=1,
+                        help='Source file to modify')
+    parser.add_argument('macros',
+                        metavar='MACRO', 
+                        type=str, 
+                        nargs='*', 
+                        help='Macros to modify')
+    parser.add_argument('--help', '-?', action='help')
+    try:
+        args = parser.parse_args(argv)
+        if len(args.macros) < 1:
+            parser.print_usage()
+            print 'At least one macro argument is required.'
+            raise
+    except:
+        return 1
+    
+    # FILE PARSING
+    try:
+        file = open(args.source[0], "r+")
+        source = file.readlines()
+    except exception as e:
+        print 'Failed to open file. ' + e.msg
+        return 1
+
+    # C++ MACRO PARSING:
+    line_number = 1
+    new_source = ""
+    for line in source:
+        if not line.strip().startswith('#define'):
+            new_source += line
+        else:
+            values = line.split(None, 3) # value[0] = #define, value[1] = macro name, value[2] = data to modify
+            if len(values) > 2 and values[1] in args.macros:
+                # Remove any quotes and trailing \0 if they exist:
+                macro_value = values[2].strip('"').replace('\\0', '')
+                # Determine the seperator being used:
+                separator = '.'
+                for c in macro_value:
+                    if not c.isdigit():
+                        separator = c
+                # Get value parts:
+                parts = macro_value.split(separator, 4)
+                # [0] Major:    This is usually left alone and changed manually by the developer,
+                #               unless it's the only part in the version; then it's incremented.
+                if len(parts) == 1:
+                    parts[0] = str(int(parts[0]) + 1)
+                # [1] Minor:    This is the last 2 digits of the current year.
+                if len(parts) > 1:
+                    parts[1] = datetime.now().strftime('%y')
+                # [2] Revision: This is the digits for the month combined with the digits for the day.
+                if len(parts) > 2:
+                    parts[2] = datetime.now().strftime('%m').strip('0') + datetime.now().strftime('%d').strip('0')
+                # [3] Build:    This is just simply incremented + 1.
+                if len(parts) > 3:
+                    parts[3] = str(int(parts[3]) + 1)
+                # Update:
+                modified_macro_value = separator.join(parts)
+                modified_line = line.replace(macro_value, modified_macro_value)
+                new_source += modified_line
+
+                print "Line %d: Updated %s from '%s' to '%s'" % (line_number, values[1], macro_value, modified_macro_value)
+            else:
+                new_source += line
+        line_number += 1
+                
+    # WRITE THE NEW SOURCE TO THE FILE & CLOSE:    
+    file.seek(0)
+    file.write(new_source)
+    file.truncate()
+    file.close()
+
+    return 0
+
+if __name__ == "__main__":
+    try:
+        sys.exit(main())
+    except SystemExit:
+        sys.exc_clear()
+    
